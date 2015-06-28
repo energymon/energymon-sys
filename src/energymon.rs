fe80d34778aafcbf72cf31973ec46eca859195b2
Connor Imes <connor.k.imes@gmail.com>
2015-06-27 19:28:30 -0700

    reworked energymon interface

--- src/energymon.rs
+++ src/energymon.rs
@@ -1,8 +1,8 @@
-use libc::{c_int, c_double, c_char};
+use libc::{c_int, c_double, c_char, c_void};
 
-pub type EnergyMonInitFn = unsafe extern fn() -> c_int;
-pub type EnergyMonReadTotalFn = unsafe extern fn() -> c_double;
-pub type EnergyMonFinishFn = unsafe extern fn() -> c_int;
+pub type EnergyMonInitFn = unsafe extern fn(em: *mut EnergyMon) -> c_int;
+pub type EnergyMonReadTotalFn = unsafe extern fn(em: *mut EnergyMon) -> c_double;
+pub type EnergyMonFinishFn = unsafe extern fn(em: *mut EnergyMon) -> c_int;
 pub type EnergyMonGetSourceFn = unsafe extern fn(*mut c_char) -> *mut c_char;
 
 #[repr(C)]
@@ -11,4 +11,5 @@ pub struct EnergyMon {
     pub fread: EnergyMonReadTotalFn,
     pub ffinish: EnergyMonFinishFn,
     pub fsource: EnergyMonGetSourceFn,
+    pub state: *mut c_void,
 }
