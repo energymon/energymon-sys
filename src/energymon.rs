1548abb6e9101af8f385dca09b2e7bfb293a9f24
Connor Imes <connor.k.imes@gmail.com>
2015-07-01 09:59:56 -0500

    Add documentation. Change Err to return static str ref instead of String

--- src/energymon.rs
+++ src/energymon.rs
@@ -1,46 +1,61 @@
 use libc::{c_int, c_double, c_char, c_void};
 use std::mem;
 
+/// Typedef for initialization function.
 pub type EnergyMonInitFn = unsafe extern fn(em: *mut EnergyMon) -> c_int;
+/// Typedef for read function.
 pub type EnergyMonReadTotalFn = unsafe extern fn(em: *mut EnergyMon) -> c_double;
+/// Typedef for cleanup function.
 pub type EnergyMonFinishFn = unsafe extern fn(em: *mut EnergyMon) -> c_int;
+/// Typedef for function to get human-readable name.
 pub type EnergyMonGetSourceFn = unsafe extern fn(*mut c_char) -> *mut c_char;
 
 #[repr(C)]
+/// Representation of native C struct `em_impl`.
 pub struct EnergyMon {
+    /// Native C function type signature that initializes the `EnergyMon`.
     pub finit: EnergyMonInitFn,
+    /// Native C function type signature that reads energy data from the `EnergyMon`.
     pub fread: EnergyMonReadTotalFn,
+    /// Native C function type signature that cleans up the `EnergyMon`.
     pub ffinish: EnergyMonFinishFn,
+    /// Native C function type signature that gets the `EnergyMon`'s human-readable name.
     pub fsource: EnergyMonGetSourceFn,
+    /// Native C pointer used by the underlying `EnergyMon` implementation for storing state.
     pub state: *mut c_void,
 }
 
 extern {
+    /// Native C function that fills in the EnergyMon struct values and may allocate other
+    /// resources.
     fn em_impl_get(em: *mut EnergyMon) -> c_int;
 }
 
 impl EnergyMon {
-    pub fn new() -> Result<EnergyMon, String> {
+    /// Create an `EnergyMon` which represents a C struct.
+    pub fn new() -> Result<EnergyMon, &'static str> {
         unsafe {
             let mut em: EnergyMon = mem::uninitialized();
             let result = em_impl_get(&mut em);
             if result != 0 {
-                return Err("Failed to initialize energymon".to_string());
+                return Err("Failed to create energymon");
             }
             Ok(em)
         }
     }
 
-    pub fn init(&mut self) -> Result<(), String> {
+    /// Initialize the `EnergyMon` - may allocate more resources and/or start threads.
+    pub fn init(&mut self) -> Result<(), &'static str> {
         let result: i32 = unsafe {
             (self.finit)(self)
         };
         if result != 0 {
-            return Err("Failed to initialize energymon".to_string());
+            return Err("Failed to initialize energymon");
         }
         Ok(())
     }
 
+    /// Read the total energy from the `EnergyMon`.
     pub fn read(&mut self) -> f64 {
         unsafe {
             (self.fread)(self)
@@ -48,22 +63,24 @@ impl EnergyMon {
         
     }
 
-    pub fn finish(&mut self) -> Result<(), String> {
+    /// Cleanup the `EnergyMon` - may deallocate associated resources and/or stop threads.
+    pub fn finish(&mut self) -> Result<(), &'static str> {
         let result: i32 = unsafe {
             (self.ffinish)(self)
         };
         if result != 0 {
-            return Err("Failed to initialize energymon".to_string());
+            return Err("Failed to cleanup energymon");
         }
         Ok(())
     }
 
-    pub fn source(&mut self) -> Result<String, String> {
+    /// Utility function to get a human-readable name of the `EnergyMon`'s source.
+    pub fn source(&mut self) -> Result<String, &'static str> {
         let mut buf = [0; 100];
         unsafe {
             let ret = (self.fsource)(buf.as_mut_ptr());
             if ret.is_null() {
-                return Err("Failed to get energymon source".to_string());
+                return Err("Failed to get energymon source");
             }
             let buf = mem::transmute::<&[c_char], &[u8]>(&buf);
             Ok(String::from_utf8_lossy(buf).into_owned())
