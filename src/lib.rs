8dc996f9dde7228bc34997a83c30f25edd5b2b51
Connor Imes <connor.k.imes@gmail.com>
2015-08-03 09:05:49 -0500

    Name refactoring in native lib

--- src/lib.rs
+++ src/lib.rs
@@ -5,29 +5,29 @@ extern crate libc;
 use libc::{c_int, c_ulonglong, c_char, c_void};
 
 /// Typedef for initialization function.
-pub type em_init_func = extern fn(*mut em_impl) -> c_int;
+pub type energymon_init = extern fn(*mut energymon) -> c_int;
 /// Typedef for read function.
-pub type em_read_total_func = extern fn(*const em_impl) -> c_ulonglong;
+pub type energymon_read_total = extern fn(*const energymon) -> c_ulonglong;
 /// Typedef for cleanup function.
-pub type em_finish_func = extern fn(*mut em_impl) -> c_int;
+pub type energymon_finish = extern fn(*mut energymon) -> c_int;
 /// Typedef for function to get human-readable name.
-pub type em_get_source_func = extern fn(*mut c_char) -> *mut c_char;
+pub type energymon_get_source = extern fn(*mut c_char) -> *mut c_char;
 /// Typedef for function to get refresh interval.
-pub type em_get_interval_func = extern fn(*const em_impl) -> c_ulonglong;
+pub type energymon_get_interval = extern fn(*const energymon) -> c_ulonglong;
 
 #[repr(C)]
-/// Representation of native C struct `em_impl`.
-pub struct em_impl {
-    /// Native C function type signature that initializes the `em_impl`.
-    pub finit: em_init_func,
-    /// Native C function type signature that reads energy data from the `em_impl`.
-    pub fread: em_read_total_func,
-    /// Native C function type signature that cleans up the `em_impl`.
-    pub ffinish: em_finish_func,
-    /// Native C function type signature that gets the `em_impl`'s human-readable name.
-    pub fsource: em_get_source_func,
-    /// Native C function type signature that gets the `em_impl`'s refresh interval.
-    pub finterval: em_get_interval_func,
-    /// Native C pointer used by the underlying `em_impl` implementation for storing state.
+/// Representation of native C struct `energymon`.
+pub struct energymon {
+    /// Native C function type signature that initializes the `energymon`.
+    pub finit: energymon_init,
+    /// Native C function type signature that reads energy data from the `energymon`.
+    pub fread: energymon_read_total,
+    /// Native C function type signature that cleans up the `energymon`.
+    pub ffinish: energymon_finish,
+    /// Native C function type signature that gets the `energymon`'s human-readable name.
+    pub fsource: energymon_get_source,
+    /// Native C function type signature that gets the `energymon`'s refresh interval.
+    pub finterval: energymon_get_interval,
+    /// Native C pointer used by the underlying `energymon` implementation for storing state.
     pub state: *mut c_void,
 }
