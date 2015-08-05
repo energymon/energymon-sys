a8d52fecf37d315befd0883fe8760c837b50ecab
Connor Imes <connor.k.imes@gmail.com>
2015-08-04 19:47:05 -0500

    New parameter for energymon_get_source function type

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 
 extern crate libc;
 
-use libc::{c_int, c_ulonglong, c_char, c_void};
+use libc::{c_int, c_ulonglong, c_char, c_void, size_t};
 
 /// Typedef for initialization function.
 pub type energymon_init = extern fn(*mut energymon) -> c_int;
@@ -11,7 +11,7 @@ pub type energymon_read_total = extern fn(*const energymon) -> c_ulonglong;
 /// Typedef for cleanup function.
 pub type energymon_finish = extern fn(*mut energymon) -> c_int;
 /// Typedef for function to get human-readable name.
-pub type energymon_get_source = extern fn(*mut c_char) -> *mut c_char;
+pub type energymon_get_source = extern fn(*mut c_char, size_t) -> *mut c_char;
 /// Typedef for function to get refresh interval.
 pub type energymon_get_interval = extern fn(*const energymon) -> c_ulonglong;
 
