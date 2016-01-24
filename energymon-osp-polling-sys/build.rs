150b99868fc6d29433bc0852fbefc9ef131db5fe
Connor Imes <connor.k.imes@gmail.com>
2016-01-24 10:04:05 -0600

    Use pkg-config on local native builds to get transitive dependencies for static libs

--- energymon-osp-polling-sys/build.rs
+++ energymon-osp-polling-sys/build.rs
@@ -6,8 +6,9 @@ use std::path::PathBuf;
 use std::process::Command;
 
 fn main() {
-    pkg_config::find_library("hidapi-libusb").unwrap();
-    match pkg_config::find_library("energymon-osp-polling-static") {
+    let lib = "energymon-osp-polling-static";
+    env::set_var("ENERGYMON_OSP_POLLING_STATIC_STATIC", "");
+    match pkg_config::find_library(&lib) {
         Ok(_) => (),
         Err(_) => {
             let src = PathBuf::from(&env::var_os("CARGO_MANIFEST_DIR").unwrap())
@@ -20,15 +21,36 @@ fn main() {
                                 src.join("cmake-toolchain").join("android.toolchain.cmake").display()),
                 false => "".to_owned(),
             };
+            let cmake_gen = match env::var("MSYSTEM") {
+                Ok(val) => {
+                    if val.contains("MINGW") {
+                        "-GMSYS Makefiles".to_owned()
+                    } else {
+                        "".to_owned()
+                    }
+                },
+                Err(_) => "".to_owned(),
+            };
             fs::remove_dir_all(&build).ok();
             fs::create_dir_all(&build).unwrap();
-            run(Command::new("cmake").arg(cmake_var).arg(src.to_str().unwrap()).current_dir(&build));
-            run(Command::new("make").arg("energymon-osp-polling-static").current_dir(&build));
-            println!("cargo:rustc-link-lib=static=energymon-osp-polling-static");
-            println!("cargo:rustc-link-search=native={}", build.join("lib").display());
+            run(Command::new("cmake").arg(cmake_var).arg(&cmake_gen).arg(src.to_str().unwrap()).current_dir(&build));
+            run(Command::new("make").arg(&lib).current_dir(&build));
+            // run pkg-config on compiled dir to get any transitive dependencies of static lib
+            set_pkg_config_path(&build, !cmake_gen.is_empty());
+            pkg_config::find_library(&lib).unwrap();
         },
     }
-    println!("cargo:rustc-flags=-l hidapi-libusb -l pthread -l rt");
+}
+
+fn set_pkg_config_path(build: &PathBuf, is_windows: bool) {
+    let delimiter = match is_windows {
+        true => ";",
+        false => ":",
+    };
+    match env::var_os("PKG_CONFIG_PATH") {
+        Some(p) => env::set_var("PKG_CONFIG_PATH", format!("{:?}{}{:?}", build, delimiter, p)),
+        None => env::set_var("PKG_CONFIG_PATH", build),
+    }
 }
 
 fn run(cmd: &mut Command) {
